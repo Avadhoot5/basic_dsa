7e07eb3df9d4d69e617327c2eb0cb1b631d40a2b
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-01 17:33:31 +0530

    increase and decrease fn - recursion

---
+++ 01_Basics_DSA/01_Recursion/01_print_decreasing.py
@@ -0,0 +1,19 @@
+# print decreasing values of n
+
+def printDecrease(n):
+    if (n == 0):
+        return 
+    print(n)
+    printDecrease(n-1)
+
+# printDecrease(5)
+
+def pritnIncrease(n):
+    if (n == 0):
+        return 
+    pritnIncrease(n-1)
+    print(n)
+
+# pritnIncrease(5)
+
+
