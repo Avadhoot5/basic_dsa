19a2738ea5b574f693417769dcb0751a01c3748c
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-02 20:49:21 +0530

    factorial using Log(n) time - recursion

--- 01_Basics_DSA/01_Recursion/04_power.py
+++ 01_Basics_DSA/01_Recursion/04_power.py
@@ -5,5 +5,18 @@ def power(x, n):
         return 1
     return x * power(x, n-1)
 
-ans = power(2, 9)
+# ans = power(2, 9)
+# print(ans)
+
+# n power x using log time
+
+def powerL(x, n):
+    if (n == 0):
+        return 1
+    if (n % 2 == 0):
+        return powerL(x, n//2) * powerL(x, n//2)
+    else:
+        return x * powerL(x, n//2) * powerL(x, n//2)
+
+ans = powerL(2, 7)
 print(ans)
