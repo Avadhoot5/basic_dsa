6ee2fc90398ee93ef605fefb2a8253c7b9e1acf6
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-06 11:03:38 +0530

    subsequence of a string - recursion

---
+++ 01_Basics_DSA/01_Recursion/13_subsequence.py
@@ -0,0 +1,21 @@
+# subsequence of a given string
+
+# abc -> result from bc. 
+
+
+def recStr(str):
+    if (len(str) == 0):
+        return ['']
+    ch = str[0]
+    ros = str[1:]
+    rres = recStr(ros)
+    mres = []
+    for i in rres:
+        mres.append((ch + i))
+    for i in rres:
+        mres.append('' + i)
+    return mres
+
+ans = recStr('abc')
+
+print(ans)
