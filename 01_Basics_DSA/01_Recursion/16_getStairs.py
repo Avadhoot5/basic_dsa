40e720edc1a2edfef1a034dd8d0d19a4d1d476cf
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-08 20:39:00 +0530

    get Stairs path - recursion

--- 01_Basics_DSA/01_Recursion/16_getStairs.py
+++ 01_Basics_DSA/01_Recursion/16_getStairs.py
@@ -1,2 +1,26 @@
 # Get Stairs Path - Question | Recursion
 
+def getStairs(n):
+
+    if (n == 0):
+        return ['']
+    if (n < 0):
+        return []
+
+    path1 = getStairs(n-1)
+    path2 = getStairs(n-2)
+    path3 = getStairs(n-3)
+
+    paths = []
+    
+    for i in path1:
+        paths.append('1' + i)
+    for i in path2:
+        paths.append('2' + i)
+    for i in path3:
+        paths.append('3' + i)
+    
+    return paths
+
+ans = getStairs(4)
+print(ans)
