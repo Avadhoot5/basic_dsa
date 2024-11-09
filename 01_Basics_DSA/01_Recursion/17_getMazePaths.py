f815e5177d68886173b23a877471bd1bd8f94b2e
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-09 23:17:26 +0530

    getMaze path - recursion

--- 01_Basics_DSA/01_Recursion/17_getMazePaths.py
+++ 01_Basics_DSA/01_Recursion/17_getMazePaths.py
@@ -2,10 +2,24 @@
 
 def getMazePath(sr, sc, dr, dc):
 
+    if (sr == dr and sc == dc):
+        return ['']
+
+    hPath = []
+    vPath = []
+
+    if (sc < dc):
+        hPath = getMazePath(sr, sc+1, dr, dc)
+    if (sr < dr):
+        vPath = getMazePath(sr+1, sc, dr, dc)
     
+    paths = []
+    for i in hPath:
+        paths.append('h' + i)
+    for i in vPath:
+        paths.append('v' + i)
 
-    pass
-
+    return paths
 
 ans = getMazePath(1,1,3,3)
 print(ans)
