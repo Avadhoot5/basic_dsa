b21d5ef744aebebeea7ff12f1e27f8f304d695d2
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-10 22:38:49 +0530

    get MazePaths Jump - recursion

--- 01_Basics_DSA/01_Recursion/18_getMazePathJump.py
+++ 01_Basics_DSA/01_Recursion/18_getMazePathJump.py
@@ -7,20 +7,20 @@ def getMazePaths(sr, sc, dr, dc):
 
     paths = []
 
-    for ms in range(1, (dr-sr)+1, dr, dc):
-        hPath = getMazePaths(sr+1, sc, dr,dc)
+    for ms in range(1, (dr-sr)+1):
+        hPath = getMazePaths(sr+ms, sc, dr,dc)
         for prevH in hPath:
-            paths.append('h' + ms + prevH)
+            paths.append('h' + str(ms) + prevH)
 
-    for ms in range(1, (dc-sc)+1, dr, dc):
-        vPath = getMazePaths(sr, sc+1, dr,dc)
+    for ms in range(1, (dc-sc)+1):
+        vPath = getMazePaths(sr, sc+ms, dr,dc)
         for prevV in vPath:
-            paths.append('v' + ms + prevV)
+            paths.append('v' + str(ms) + prevV)
 
-    for ms in range(1, (dc-sc)+1 and (dr-sr)+1, dr, dc):
-        dPath = getMazePaths(sr+1, sc+1, dr,dc)
+    for ms in range(1, (dc-sc)+1 and (dr-sr)+1):
+        dPath = getMazePaths(sr+ms, sc+ms, dr,dc)
         for prevD in dPath:
-            paths.append('d' + ms + prevD)
+            paths.append('d' + str(ms) + prevD)
 
     return paths
 
