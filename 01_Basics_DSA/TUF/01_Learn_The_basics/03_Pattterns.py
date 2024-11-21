08d694a06557cfed478891e2fbca3181a6e8c7ef
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-21 14:06:43 +0530

    Star patterns till 8th

--- 01_Basics_DSA/TUF/01_Learn_The_basics/03_Pattterns.py
+++ 01_Basics_DSA/TUF/01_Learn_The_basics/03_Pattterns.py
@@ -31,3 +31,36 @@ def triNum2(n):
     
 # triNum2(5)
 
+def opTri(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+# opTri(5)
+
+def numTri(n):
+    for i in range(n, 0, -1):
+        val = ''
+        for j in range(1, i+1):
+            val += str(j)
+        print(val)
+
+# numTri(5)
+
+def centerTri(n):
+    for i in range(1, n+1):
+        print(' ' * (n-i), end = '')
+        print('*' * ((2*i)-1))
+
+# centerTri(5)
+
+def centerTriDown(n):
+    for i in range(1, n+1):
+        print('*' * ((2*(n-i))+1))
+        print(' ' * (i), end = '')
+
+centerTriDown(5)
+
+
+
+
+
