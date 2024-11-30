ebc5ea56fd65b58b3b2ec64da926d865303bc030
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-11-30 19:34:41 +0530

    multiple recursion calls, palindrome optimal - recrusion

--- 01_Basics_DSA/02_BasicMaths/02_Recursion.py
+++ 01_Basics_DSA/02_BasicMaths/02_Recursion.py
@@ -84,5 +84,110 @@ def fact(n):
         return 1
     return n * fact(n-1)
 
-print(fact(4))
+# print(fact(4))
+
+# print fact for every number less than or equal to N
+
+def factPrint(n):
+    if (n==0):
+        return 1
+    value = factPrint(n-1)
+    print(value * n, end = ' ')
+    return value * n
+
+# factPrint(3)
+
+# reverse an array
+
+arr = [1, 4, 3, 2, 6, 5]
+
+# using for loop
+
+def revFor(arr):
+    newArr = []
+    for i in range(len(arr)-1, -1, -1):
+        newArr.append(arr[i])
+    return newArr
+
+# print(revFor(arr))
+
+
+def revTwoPointer(l, r, arr):
+    if (l >= r):
+        return
+    arr[l], arr[r] = arr[r], arr[l]
+    revTwoPointer(l+1, r-1, arr)
+    return arr
+
+# print(revTwoPointer(0, 5, arr))
+
+def revOne(i, n, arr):
+    if (i >= n//2):
+        return
+    arr[i], arr[n-i] = arr[n-i], arr[i]
+    revOne(i+1, n, arr)
+    return arr
+
+# print(revOne(0, 5, arr))
+
+# check if a string is palindrome or not.
+
+def palindrome(i, s):
+    if (i >= len(s)//2):
+        return 'Palindrome'
+    if(s[i] != s[len(s)-i-1]):
+        return 'Not Palindrome'
+    else:
+        return palindrome(i+1, s)
+
+# print(palindrome(0,'teset'))
+
+# Leetcode question - 
+
+def palindromeLC(s):
+    ans = ''
+    for i in s:
+        if (i.isalnum()):
+            ans += i
+    return ans.lower() == ans[::-1].lower()
+
+# result = palindromeLC('A man, a plan, a canal: Panama')
+# print(result)
+
+# Optimal solution
+
+def palindromeLCO(s):
+    l = 0
+    r = len(s) - 1
+
+    def alphaNum(c):
+        (ord('A') <= ord(c) <= ord('Z') or
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9'))
+
+    while(l < r):
+        while l < r and not alphaNum(s[l]):
+            l += 1
+        while r > l and not alphaNum(s[r]):
+            r -= 1
+        if (s[l] != s[r]):
+            return False
+        l, r = l+1, r-1
+    return True
+
+# result = palindromeLC('A man, a plan, a canal: Panama')
+# print(result)
+
+# Multiple recursion calls
+
+def fibo(n):
+    if (n == 0):
+        return 0
+    if (n == 1):
+        return 1
+    last = fibo(n-1)
+    secondLast = fibo(n-2)
+    return last + secondLast
+
+# print(fibo(5))
 
