23738ee7722a8d5cfa5b095a4faec866437face8
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-12-01 19:36:33 +0530

    basic hashing learnt - freq of elements

--- 01_Basics_DSA/TUF/01_Learn_The_basics/04_hashing.py
+++ 01_Basics_DSA/TUF/01_Learn_The_basics/04_hashing.py
@@ -14,7 +14,7 @@ def countNum(n):
             hashArr[arr[i]] = 1
     return hashArr.get(n)
 
-print(countNum(3))
+# print(countNum(3))
 
 alphaArr = ['a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'c', 'd', 'd', 'd', 'd', 'e', 'e']
 
@@ -28,5 +28,30 @@ def charHash(ch):
             alphaHash[i] = 1
     return alphaHash[ch]
 
-print('Character appears')
-print(charHash('d'))
+# print('Character appears')
+# print(charHash('d'))
+
+# # Hashing - methods 
+# 1. Division Method
+# 2. Folding Method
+# 3. Mid square Method
+
+# 1. Frequencies of Limited Range Array Elements
+# Output: [0, 2, 2, 0, 1] Explanation: We have: 1 occurring 0 times, 2 occurring 2 times, 3 occurring 2 times, 4 occurring 0 times, and 5 occurring 1 time.
+
+# arr = [2, 3, 2, 3, 5]
+inputArr = [2, 1, 1]
+
+def freqElements(arr):
+    hashArr = [0] * (len(arr) + 1)
+
+    for i in arr:
+        hashArr[i] += 1
+
+    return hashArr[1:]
+
+print(freqElements(inputArr))
+
+
+# 2. Find highest/lowest frequency element
+
