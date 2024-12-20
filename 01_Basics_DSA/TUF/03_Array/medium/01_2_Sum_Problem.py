2e23001f58b730733f9441247a25e01bf2d682fc
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-12-20 12:16:29 +0530

    two sum problem - brute approach

---
+++ 01_Basics_DSA/TUF/03_Array/medium/01_2_Sum_Problem.py
@@ -0,0 +1,41 @@
+# Two sum problem 
+
+# arr = [], target = k
+
+# two types - 1. If sum exists return Yes/No. 2. Sum do exist return the indexes of the elements
+
+inputArr = [2, 6, 5, 8, 11]
+
+# TC. O(N*N). SC. O(1)
+
+def twoSumProblemBF(arr, k):
+    n = len(arr)
+    for i in range(0, n):
+        for j in range(0, n):
+            if (i == j):
+                continue
+            if ((arr[i] + arr[j]) == k):
+                return i, j
+    return 'No'
+
+# print(twoSumProblemBF(inputArr, 13))
+
+# TC. O(N*N). bit less than O(N2) 
+
+def twoSumProblemBF2(arr, k):
+    n = len(arr)
+    for i in range(0, n):
+        for j in range(i+1, n):
+            if (arr[i] + arr[j] == k):
+                return i, j
+    return -1
+
+# print(twoSumProblemBF2(inputArr, 17))
+
+# def twoSumProblemB(arr, k):
+#     hashMap = {}
+
+#     # for i in range(0, len(arr)):
+#     #     if (arr[i])
+
+# print(twoSumProblemB(inputArr, 17))
