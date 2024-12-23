18663e5c5d44b05b025e1f4afe92f097af67e5ef
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-12-23 11:22:38 +0530

    practiced longest subarray with sum K

--- 01_Basics_DSA/TUF/03_Array/easy/test.py
+++ 01_Basics_DSA/TUF/03_Array/easy/test.py
@@ -15,7 +15,30 @@ def removeDup(arr):
 
 # Intersection
 
+arr1 = [1, 2, 2, 3, 4, 5, 6]
+arr2 = [2, 3, 5, 6, 7, 8]
 
 
 
+# longest subarray with sum k - optimal
 
+longArrB =[1, 2, 3, 0, 0, 1, 1, 1, 4, 2, 3]
+
+def longestSub(arr, k):
+    n = len(arr)
+    left, right = 0, 0
+    length = 0
+    sum = 0
+
+    while (right < n):
+        while (left <= right and sum > k):
+            sum = sum - arr[left]
+            left+=1
+        if (sum == k):
+            length = max(length, right-left + 1)
+        right+=1
+        if (right < n):
+            sum += arr[right]
+    return length
+
+print(longestSub(longArrB, 3))
