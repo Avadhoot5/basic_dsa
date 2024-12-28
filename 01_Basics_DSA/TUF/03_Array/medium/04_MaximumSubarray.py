85ee0972c5d065a7d09bf11119da20fb703722d8
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-12-28 21:03:39 +0530

    maxSubarray Kadane's algo

--- 01_Basics_DSA/TUF/03_Array/medium/04_MaximumSubarray.py
+++ 01_Basics_DSA/TUF/03_Array/medium/04_MaximumSubarray.py
@@ -1,3 +1,67 @@
 # Kadane's Algorithm | Maximum Subarray Sum | Finding and Printing
 
+inputArr = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+# brute force approach. TC. O(N*N*N). SC. O(1)
+
+# better approach TC. O(N*N). SC. O(1) 
+
+def maxSubarraySumB(arr):
+    maxSum = 0
+    n = len(arr)
+
+    for i in range(0, n):
+        currentSum = 0
+        for j in range(i, n):
+            currentSum += arr[j]
+            if (currentSum > maxSum):
+                maxSum = currentSum
+    return maxSum
+
+# print(maxSubarraySumB(inputArr))
+
+# optimal Kadane's algo
+
+def maxSubarraySumO(arr):
+    maxSum = 0
+    n = len(arr)
+    currentSum = 0
+
+    for i in range(0, n):
+        currentSum += arr[i]
+
+        if (currentSum > maxSum):
+            maxSum = currentSum
+
+        if (currentSum < 0):
+            currentSum = 0
+
+    return maxSum
+
+print(maxSubarraySumO(inputArr))
+
+# follow up que - print the subarray 
+
+
+def maxSubarraySumO2(arr):
+    maxSum = 0
+    n = len(arr)
+    currentSum = 0
+    arrStart, arrEnd = -1, -1
+
+    for i in range(0, n):
+        if (currentSum == 0):
+            start = i
+        currentSum += arr[i]
+        if (currentSum > maxSum):
+            maxSum = currentSum
+            arrStart = start
+            arrEnd = i
+
+        if (currentSum < 0):
+            currentSum = 0
+
+    return maxSum, arrStart, arrEnd
+
+print(maxSubarraySumO2(inputArr))
 
