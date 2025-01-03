d2cd0f414a27e4f7a038046efcd0aec02b2d0241
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-01-03 19:56:08 +0530

    next Permutation my implementation

--- 01_Basics_DSA/TUF/03_Array/medium/07_NextPermutate.py
+++ 01_Basics_DSA/TUF/03_Array/medium/07_NextPermutate.py
@@ -7,8 +7,55 @@
 
 # printing permutation using recursion.
 
-def nextPerm(value):
+def nextPermBF(value):
     pass
 
-print(nextPerm(312))
+# print(nextPermBF(312))
+
+nextArr = [2, 1, 5, 4, 3, 0, 0]
+nextArr2 = [1, 2, 3]
+
+# Self tried algorithm 
+
+def nextPermOMine(arr):
+    breakPoint = -1
+    n = len(arr)
+    elementIdx = -1
+    currentValue = [i for i in arr]
+    finalValue = []
+
+    for i in range(0, n):
+        if (arr[i] < arr[i+1]):
+            breakPoint = i
+            break
+    if (breakPoint != -1):
+        smallest = arr[breakPoint+1]
+        for j in range(breakPoint+1, n):
+            if (arr[j] > arr[breakPoint]):
+                if (smallest > arr[j]):
+                    smallest = arr[j]
+                    elementIdx = j
+        currentValue[breakPoint], currentValue[elementIdx] = currentValue[elementIdx], currentValue[breakPoint]
+
+        # adding the elements till the breakpoint
+        finalValue += currentValue[:breakPoint+1]
+        print(finalValue)
+        subList = currentValue[breakPoint+1:]
+        subList.sort()
+        finalValue += subList
+        print(finalValue)
+
+    else:
+        arr.sort()
+    
+    return finalValue
+
+
+print(nextPermOMine(nextArr))
+print(nextPermOMine(nextArr2))
+
+def nextPermO(arr):
+    pass
+
+print(nextPermO(nextArr))
 
