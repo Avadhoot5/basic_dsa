f81d63febf939906a8a247b71687e93b4ffc420e
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-01-06 12:20:30 +0530

    longest consec. seq. optimal

--- 01_Basics_DSA/TUF/03_Array/medium/09_LongestConsecutiveSeq.py
+++ 01_Basics_DSA/TUF/03_Array/medium/09_LongestConsecutiveSeq.py
@@ -5,6 +5,52 @@ nums2 = [102,4,100,1,101,1,3,2]
 
 # brute force approach.
 
+def linearSearch(a, x):
+    for i in range(len(a)):
+        if (a[i] == x):
+            return True
+    return False
+
+def longestSqBF1(arr):
+    n = len(arr)
+    longest = 1
+
+    for i in range(0, n):
+        cnt = 0
+        x = arr[i]
+        while (linearSearch(arr, x) == True):
+            x += 1
+            cnt += 1
+        longest = max(longest, cnt)
+    return longest
+
+# print(longestSqBF1(nums))
+
+# Better.
+
+def longestSeqB(arr):
+    arr.sort()
+    if (len(arr) == 0):
+        return 0
+    n = len(arr)
+    longest = 1
+    lastSmall = -1000000
+    cnt = 0
+
+    for i in range(0, n):
+        if (arr[i]-1 == lastSmall):
+            cnt += 1
+            lastSmall = arr[i]
+        elif (arr[i] != lastSmall):
+            cnt = 1
+            lastSmall = arr[i]
+        longest = max(cnt, longest)
+    return longest
+
+# print(longestSeqB(nums))
+
+# my approach 
+
 def longestSqBF(arr):
     arr = list(set(arr))
     arr.sort()
@@ -27,4 +73,26 @@ def longestSqBF(arr):
 
 # optimal approach 
 
+def longestSqO(arr):
+    n = len(arr)
+    setArr = set()
+    for i in range(0, n):
+        setArr.add(arr[i])
+    
+    longest = 1
+    
+    for i in setArr:
+        if (i-1 not in setArr):
+            x = i
+            cnt = 1
+            while (x+1 in setArr):
+                x += 1
+                cnt += 1
+            longest = max(longest, cnt)
+    
+    return longest
+
+
+print(longestSqO(nums))
+print(longestSqO(nums2))
 
