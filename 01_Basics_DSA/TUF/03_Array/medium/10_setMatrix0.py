0bd0c16522e0d4b8697aa7c0b1815661eb3780c3
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-01-07 17:36:06 +0530

    set matrix 0s optimal

--- 01_Basics_DSA/TUF/03_Array/medium/10_setMatrix0.py
+++ 01_Basics_DSA/TUF/03_Array/medium/10_setMatrix0.py
@@ -63,6 +63,37 @@ print(setMatrixZeroB(matrixB, len(matrixB), len(matrixB[0])))
 # optimal approach 
 
 def setMatrixZeroO(matrix, n , m):
-    pass
+    # colM = [0] * m -> matrix[0][..]
+    # rowM = [0] * n -> matrix[..][0]
+    colZero = 1
 
-print(setMatrixZeroO(matrixB, len(matrixB), len(matrixB[0])))
+    for i in range(0, n):
+        for j in range(0, m):
+            if (matrix[i][j] == 0):
+
+                # mark the i-th row
+                matrix[i][0] = 0
+                # mark the j-th col
+                if (j != 0):
+                    matrix[0][j] = 0
+                else:
+                    colZero = 0
+    for i in range(1, n):
+        for j in range(1, m):
+            if (matrix[i][j] != 0):
+                if (matrix[i][0] == 0 or matrix[0][j] == 0):
+                    matrix[i][j] = 0
+    
+    if (matrix[0][0] == 0):
+        for j in range(m):
+            matrix[0][j] = 0
+
+    if (colZero == 0):
+        for i in range(n):
+            matrix[i][0] = 0
+
+    return matrix
+    
+ans = setMatrixZeroO(matrixB, len(matrixB), len(matrixB[0]))
+print(ans)
+
