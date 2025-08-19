69b5817a60c8656a604acdfbf46a7a91f6a72222
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-08-19 23:23:37 +0530

    merge sort, quick sort DONE

--- 01_Basics_DSA/TUF/02_Sorting/pratice_Sorting.py
+++ 01_Basics_DSA/TUF/02_Sorting/pratice_Sorting.py
@@ -31,9 +31,9 @@ def bubbleSort(arr):
             if (arr[j] > arr[j+1]):
                 didSwap = 1
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-            if (didSwap == 0):
-                break
-            print('runs')
+        if (didSwap == 0):
+            break
+        print('runs')
     return arr
 
 # print(bubbleSort(bubbleArr))
@@ -53,4 +53,119 @@ def insertionSort(arr):
 
     return arr
 
-print(insertionSort(insertArr))
+# print(insertionSort(insertArr))
+
+# Merge Sort - divide and merge
+
+# Two merge sort - uses iterative method
+
+# Merge - Two sorted list into one list that is also sorted 
+# Algorithm 
+
+arr1 = [9, 13, 20, 24, 46, 52]
+arr2 = [2, 3, 5, 6, 8, 16]
+
+def twoWayMerge(A, B, m, n):
+    i, j, k = 0, 0, 0
+    temp = [0] * (m + n)
+
+    while (i < m and j < n):
+        if (A[i] <= B[j]):
+            temp[k] = A[i]
+            i += 1
+            k += 1
+        else:
+            temp[k] = B[j]
+            j += 1
+            k += 1
+    for rem in range(i, m):
+        temp[k] = A[rem]
+        k += 1
+    
+    for rem in range(j, n):
+        temp[k] = B[rem]
+        k += 1
+    return temp
+
+# print(twoWayMerge(arr1, arr2, len(arr1), len(arr1)))
+
+# Merge Sort - uses recursive method
+
+mergeArr = [3,4,1,6,2,5,7]
+
+def merge(arr, low, mid, high):
+    left = low
+    right = mid + 1
+    temp = []
+
+    while (left <= mid and right <= high):
+        if (arr[left] <= arr[right]):
+            temp.append(arr[left])
+            left += 1
+        else:
+            temp.append(arr[right])
+            right += 1
+    while (left <= mid):
+        temp.append(arr[left])
+        left += 1
+    while (right <= high):
+        temp.append(arr[right])
+        right += 1
+
+    for i in range(low, high):
+        arr[i] = temp[i - low]
+
+def mergeS(arr, low, high):
+    if (low >= high):
+        return
+    mid = (low + high) // 2
+    mergeS(arr, low, mid)
+    mergeS(arr, mid + 1, high)
+    merge(arr, low, mid, high)
+
+def mergeSort(arr, n):
+    mergeS(arr, 0, n-1)
+
+# mergeSort(mergeArr, len(mergeArr))
+# print(mergeArr)
+
+# Quick sort
+quickArr = [4, 6, 2, 5, 7, 9, 1, 3]
+
+
+def partition(arr, low, high):
+    pivot = arr[low]
+    i = low
+    j = high
+
+    while (i < j):
+        while (arr[i] >= pivot and i <= high-1):
+            i += 1
+        while (arr[j] < pivot and j >= low + 1):
+            j -= 1
+        if (i < j):
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[low], arr[j] = arr[j], arr[low]
+    
+    return j
+
+def quickSort(arr, low, high):
+    if (low < high):
+        pIndex = partition(arr, low, high)
+        quickSort(arr, low, pIndex-1)
+        quickSort(arr, pIndex+1, high)
+        return arr
+
+ans = quickSort(quickArr, 0, len(quickArr)-1)
+print(ans)
+
+
+
+# Recursive Bubble Sort Algorithm
+
+
+# Recursive Insertion Sort Algorithm
+
+
+
+
