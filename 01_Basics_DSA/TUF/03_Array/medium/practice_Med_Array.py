ac2128a491bc574877966ee5bad060b19a32e69d
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-09-04 17:59:53 +0530

    Majority Element, kadanes algo,stock buy and sell

--- 01_Basics_DSA/TUF/03_Array/medium/practice_Med_Array.py
+++ 01_Basics_DSA/TUF/03_Array/medium/practice_Med_Array.py
@@ -113,20 +113,155 @@ majorityArr = [2, 2, 3, 3, 1, 2, 2]
 
 def majorityElementBF(arr):
     n = len (arr)
+    for i in range(0, n):
+        count = 0
+        for j in range(0, n):
+            if (arr[i] == arr[j]):
+                count += 1
+        if (count > (n//2)):
+            return arr[i]
+    return -1
 
-
-
-print(majorityElementBF(majorityArr))
-
-
+# print(majorityElementBF(majorityArr))
 
 
 def majorityElementB(arr):
+    n = len(arr)
+    hashMap = {}
+
+    for i in range(0, n):
+        hashMap[arr[i]] = hashMap.get(arr[i], 0) + 1
+    
+    for i in hashMap:
+        if (hashMap[i] > n//2):
+            return i
+
+# print(majorityElementB(majorityArr))
+
+def majorityElement(arr):
+    count, element = 0,0
+    n = len(arr)
+
+    for i in range(n):
+        if (count == 0):
+            count = 1
+            element = arr[i]
+        elif (element == arr[i]):
+            count += 1
+        else:
+            count -=1
+    
+    maxCount = 0
+    for i in range(n):
+        if (arr[i] == element):
+            maxCount += 1
+        if (maxCount > n//2):
+            return arr[i]
+    return -1
+
+# print(majorityElement(majorityArr))
+
+# Kadane's Algorithm | Maximum Subarray Sum | Finding and Printing
+
+maxSubArr = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+# brute force approach. TC. O(N*N*N). SC. O(1)
+
+# better approach TC. O(N*N). SC. O(1) 
+
+def maxSubarraySumB(arr):
+    n = len(arr)
+    maxSum = arr[0]
+
+    for i in range(0, n):
+        currentSum = 0
+        for j in range(i, n):
+            currentSum += arr[j]
+            maxSum = max(maxSum, currentSum)
+    return maxSum
+
+# print(maxSubarraySumB(maxSubArr))
+
+import sys
+min_int = -sys.maxsize - 1
+
+def maxSubarraySumO(arr):
+    n = len(arr)
+    maxSum = min_int
+    currentSum = 0
+
+    for i in range(n):
+        currentSum += arr[i]    
+
+        if (currentSum > maxSum):
+            maxSum = currentSum
+
+        if (currentSum < 0):
+            currentSum = 0
+
+    return maxSum
+
+# print(maxSubarraySumO(maxSubArr))
+
+# follow up que - print the subarray 
+
+def maxSubarraySumO2(arr):
+    n = len(arr)
+    maxSum = min_int
+    currentSum = 0
+    start, startIdx, endIdx = -1, -1, -1
+
+    for i in range(n):
+        if (currentSum == 0):
+            start = i
+        currentSum += arr[i]    
+
+        if (currentSum > maxSum):
+            maxSum = currentSum
+            startIdx = start
+            endIdx = i
+
+        if (currentSum < 0):
+            currentSum = 0
+
+    return [startIdx, endIdx]
+
+print(maxSubarraySumO2(maxSubArr))
+
+# stock buy and sell
+
+#  Best Time to Buy and Sell Stock 
+
+# TC => O(N). SC. O(1)
+
+# stockArrN = [7,6,4,3,1]
+
+stockArr = [7, 1, 5, 3, 6, 4]
+
+def stockArrB(arr):
+    n = len(arr)
+    minimum = arr[0]
+    profit = 0
+
+    for i in range(1, n):
+        cost = arr[i] - minimum
+        profit = max(cost, profit)
+        minimum = min(minimum, arr[i])
+    return profit
+
+# print(stockArrB(stockArr))
+
+# Rearrange Array Elements by Sign | 2 Varieties of same Problem
+
+reArr = [3, 1, -2, -5, 2, -4]
+
+# TC. O(N + N/2). SC. O(N)
+
+def reArrangeBF(arr):
     pass
 
 
-
-
+print(reArrangeBF(reArr))
 
 
 
