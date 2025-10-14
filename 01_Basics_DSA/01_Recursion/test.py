69018a4bcb8e95f6fae06cac85bb72ec85ab75d3
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-10-14 11:28:43 +0530

    factorial log time, zig zag, tower of hanoi

--- 01_Basics_DSA/01_Recursion/test.py
+++ 01_Basics_DSA/01_Recursion/test.py
@@ -24,6 +24,62 @@ def printIncreaseDecrease(n):
 
 # printIncreaseDecrease(5)
 
+# factorial of n 
+
+def factorial(n):
+    if (n == 1):
+        return 1
+    ans = factorial(n-1)
+    res = ans * n
+    return res
+
+# print(factorial(5))
+
+# power of n
+
+def powX(x, n):
+    if (n == 0): return 1
+    return x * powX(x, n-1)
+
+# print(powX(5, 3))
+
+# power of n log time 
+
+def powLog(x, n):
+    if (n == 0): return 1
+    half = powLog(x, n // 2)
+    if (n % 2 == 0):
+        return half * half
+    else:
+        return x * half * half
+
+# print(powLog(5, 3))
+
+# Print zig zag
+
+def pred(n):
+    if (n == 0): return
+    print('Pre' + str(n))
+    pred(n-1)
+    print('In' + str(n))
+    pred(n-1)
+    print('Post' + str(n))
+
+# pred(2)
+
+# tower of hanoi
+
+def toh(n, A, B, C):
+    if (n == 0):
+        return
+    toh(n-1, A, C, B)
+    print(n, A, B)
+    toh(n-1, C, B, A)
+
+# toh(3, 'A','B','C')
+
+
+
 
 
 
