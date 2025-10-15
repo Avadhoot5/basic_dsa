9176d26047cb2db7add1268cf4917c0fe8cb3f7d
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-10-15 12:48:16 +0530

    array- display,reverse, first occr, last occr

--- 01_Basics_DSA/01_Recursion/test.py
+++ 01_Basics_DSA/01_Recursion/test.py
@@ -78,6 +78,74 @@ def toh(n, A, B, C):
 
 # toh(3, 'A','B','C')
 
+# display an array 
+
+# display an array using recursion
+
+dislpayArr = [42,7,19,3,88,56,14,27,65,91]
+
+def showArray(arr, idx):
+    if (idx == len(arr)): return
+    print(arr[idx])
+    showArray(arr, idx+1)
+
+# showArray(dislpayArr, 0)
+
+def showArrayRev(arr, idx):
+    if (idx == len(arr)): return
+    showArrayRev(arr, idx+1)
+    print(arr[idx])
+
+# showArrayRev(dislpayArr, 0)
+
+# print the maxium element of an array.
+
+maxArr = [42,7,19,3,88,56,91,14,27,65]
+
+def maxVal(arr, i):
+    if (i == len(arr)-1): return arr[i]
+    mesa = maxVal(arr, i+1)
+    if (arr[i] > mesa):
+        return arr[i]
+    else:
+        return mesa
+
+# print(maxVal(maxArr, 0))
+
+# First index ques.
+
+firstArr = [2,5,7,8,2,6,8,1]
+
+def firstOccr(arr, i, element):
+    if (i == len(arr)): return -1
+    if (arr[i] == element):
+        return i
+    else:
+        return firstOccr(arr,i+1, element)
+
+print(firstOccr(firstArr, 0, 8))
+
+
+# Last index ques.
+lastArr = [2,5,7,8,2,6,8,1]
+
+def lastOccr(arr, i, element):
+    if (i == len(arr)): return -1
+    value = lastOccr(arr, i+1,element)
+    if (value == -1):
+        if (element == arr[i]):
+            return i
+        else:
+            return -1
+    return value
+
+# print(lastOccr(lastArr, 0, 8))
+
+# return all indices of an element in an array.
+
+allArr = [2,5,7,8,2,6,9,2,8,1]
+
+
 
 
 
