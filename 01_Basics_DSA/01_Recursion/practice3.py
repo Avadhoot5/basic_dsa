3efd988128e953af0dd7531b6a453725d6b285f9
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2025-11-03 10:23:58 +0530

    recursion - fact, power using log time, predict the o/p

--- 01_Basics_DSA/01_Recursion/practice3.py
+++ 01_Basics_DSA/01_Recursion/practice3.py
@@ -26,4 +26,50 @@ def recDecInc(n):
 
 # recDecInc(8)
 
+# Factorial - Question | Recursion 
 
+def fact(n):
+    if (n == 0 or n == 1):
+        return 1
+    return n * fact(n-1)
+
+# print(fact(5))
+
+# n power x
+
+def pow(x,n):
+    if (n == 0):
+        return 1
+    return x * (pow(x, n-1))
+
+# print(pow(2, 5))
+
+# n power x using log time
+
+def powL(x, n):
+    if (n == 0): return 1
+    if (n % 2 == 0):
+        return powL(x, n//2) * powL(x, n//2)
+    else:
+        return x * powL(x, n//2) * powL(x, n//2)
+
+# print(powL(2, 5))
+
+# predict the output
+
+def pred(n):
+    if (n == 0):
+        return 
+    print('Pre' + str(n))
+    pred(n-1)
+    print('In' + str(n))
+    pred(n-1)
+    print('Post' + str(n))
+
+# pred(2)
+
+# tower of hanoi
+def towerOfHanoi(n, A, B, C):
+    pass
+
+# towerOfHanoi(2, 'A', 'B', 'C')
