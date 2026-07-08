c6006693c63ddd439d30004dc2c1903c0615fdf6
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2026-07-08 15:14:37 +0530

    Majority element, kadane's algo, print subarray

--- 01_Basics_DSA/TUF/03_Array/medium/practice_medium_2.py
+++ 01_Basics_DSA/TUF/03_Array/medium/practice_medium_2.py
@@ -113,11 +113,135 @@ def sort_arr_num_opm(arr):
 
 # print(sort_arr_num_opm(sort_arr_012))
 
+# Majority Element I | Brute-Better-Optimal | Moore's Voting Algorithm 
 
+majority_arr = [2, 2, 3, 3, 1, 2, 2]
 
+# Brute [TC: O(N*N), SC: O(1)]
 
+def majority_element_brute(arr):
+    n = len(arr)
 
+    for i in range(n):
+        count = 0
+        for j in range(n):
+            if (arr[i] == arr[j]):
+                count += 1        
+        if (count > (n//2)):
+            return arr[i]
+    return -1
 
+# print(majority_element_brute(majority_arr))
+
+# Better. [TC: O(2N), SC: O(N)]
+
+def majority_element_better(arr):
+    n = len(arr)
+    hash_arr = {}
+
+    for i in range(n):
+        if (arr[i] in hash_arr): hash_arr[arr[i]] += 1
+        else: hash_arr[arr[i]] = 1
+
+    for i in hash_arr:
+        if (hash_arr[i]  > n//2):
+            return i
+    return -1
+
+# print(majority_element_better(majority_arr))
+
+# Optimal. [TC: O(N), SC: O(1)]
+
+def majority_element_opt(arr):
+    n = len(arr)
+    count = 0
+
+    for i in range(n):
+        if (count == 0):
+            element = arr[i]
+            count = 1
+        else:
+            if (arr[i] == element):
+                count += 1
+            else:
+                count -= 1
+
+    max_count = 0   
+
+    for i in range(n):
+        if (arr[i] == element):
+            max_count += 1
+        if (max_count > n//2):
+            return element    
+
+    return -1
+
+# print(majority_element_opt(majority_arr))
+
+# Kadane's Algorithm | Maximum Subarray Sum | Finding and Printing
+
+max_sub_arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+max_sub_arr_1 = [-2, -1]
+
+import sys
+min_int = - sys.maxsize
+
+# Brute [TC: O(N*N), SC: O(1)]
+
+def max_subarr_brute(arr):
+    n = len(arr)
+    max_sum = min_int
+
+    for i in range(n):
+        curr_sum = 0
+        for j in range(i, n):
+            curr_sum += arr[j]
+            max_sum = max(max_sum, curr_sum)
+    
+    return max_sum
+
+# print(max_subarr_brute(max_sub_arr))
+
+# Type 1: Return the max - Kadane's Algorithm [TC: O(N), SC: O(1)]
+
+def max_subarr_opt_1(arr):
+    n = len(arr)
+    max_sum = min_int
+    curr_sum = 0
+
+    for i in range(n):
+        curr_sum += arr[i]
+        max_sum = max(max_sum,curr_sum)
+        if (curr_sum < 0):
+            curr_sum = 0
+
+    return max_sum
+
+# print(max_subarr_opt_1(max_sub_arr))
+
+# Type 2: Return the sub-array [TC: O(N), SC: O(1)]
+
+def max_subarr_opt_2(arr):
+    n = len(arr)
+    max_sum = min_int
+    curr_sum = 0
+    start_idx, end_idx = -1, -1
+
+    for i in range(n):
+        if (curr_sum == 0):
+            start = i            
+        curr_sum += arr[i]
+        if (curr_sum > max_sum):
+            max_sum = curr_sum
+            start_idx = start 
+            end_idx = i
+        if (curr_sum < 0):
+            curr_sum = 0
+
+    return (start_idx, end_idx)
+
+# print(max_subarr_opt_2(max_sub_arr))
 
 
 
