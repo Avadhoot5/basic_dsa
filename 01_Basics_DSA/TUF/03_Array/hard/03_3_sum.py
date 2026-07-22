2c68a84218d2e057ef4679558302317e27199ea5
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2026-07-22 11:41:25 +0530

    Array hard: 3 sum - better, optimal

--- 01_Basics_DSA/TUF/03_Array/hard/03_3_sum.py
+++ 01_Basics_DSA/TUF/03_Array/hard/03_3_sum.py
@@ -21,5 +21,53 @@ def three_sum_brute(arr):
 
 # print(three_sum_brute(que_arr))
 
+def three_sum_better(arr):
+    n = len(arr)
+    ans_set = set()
+
+    for i in range(n):
+        hash_set = set()
+        for j in range(i+1, n):
+            third = -(arr[i] + arr[j])
+            if (third in hash_set):
+                temp = tuple(sorted([arr[i], arr[j], third]))
+                ans_set.add(temp)
+
+            hash_set.add(arr[j])
+
+    return [list(ans) for ans in ans_set]
+
+print(three_sum_better(que_arr))
+
+que_arr_2 = [0,2,2,-1,-1,-2,-2,0,-1,0,2,2,-2]
+
+def three_sum_opm(arr):
+    arr.sort()
+    n = len(arr)
+    ans = []
+
+    for i in range(0, n):
+        if (i > 0 and arr[i] == arr[i-1]):
+            continue
+
+        j = i + 1
+        k  = n-1
+
+        while (j < k):
+            curr_sum = arr[i] + arr[j] + arr[k]
+            if (curr_sum < 0):
+                j += 1
+            elif (curr_sum > 0):
+                k -= 1
+            else:
+                ans.append([arr[i], arr[j], arr[k]])
+                j += 1
+                k -= 1
+                while (j < k and arr[j] == arr[j-1]): j+=1
+                while (j < k and arr[k] == arr[k+1]): k-=1
+    return ans
+
+print('Que Arr 1:', three_sum_opm(que_arr))
+print('Que Arr 2:', three_sum_opm(que_arr_2))
 
 
